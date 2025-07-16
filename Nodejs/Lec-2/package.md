4f1401c473ea4d798017b1fe3cc0ff421772df56
Divanshu Garg <garg20divanshu@gmail.com>
2025-07-16 14:18:51 +0530

    nodejs

---
+++ Nodejs/Lec-2/package.md
@@ -0,0 +1,8 @@
+# Only in package.json
+
+## if version 5.0.2 have (^) it is called caret only minor and patch will update
+
+## if version 5.0.2 have (~) it is called tilde only patch will get updated
+
+#
+# package-lock.json have all stable versions
---
+++ Nodejs/Lec-2/server.js
@@ -0,0 +1,29 @@
+const sum = require("./sumOfTwo")
+
+console.log(sum(2, 5));
+
+
+//--------------SYNC-------------------
+
+var a  = 20;
+
+function something(){
+    console.log("Something"); 
+}
+
+function something2(){
+    console.log("Something2"); 
+}
+
+function something3(){
+    console.log("Something3"); 
+}
+
+something();
+console.log(a);
+something2()
+something3()
+
+// ----------------ASYNC------------------
+
+
